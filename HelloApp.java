de19e22f95c795bd110ce8f8c3e650b2f9cdae24
it-me-kkaran11 <rkarunakaran174@gmail.com>
2026-03-12 21:24:25 +0530

    UC1 HelloApp implemented

---
+++ HelloApp.java
@@ -0,0 +1,21 @@
+/**
+ * HelloApp.java – A simple Java application that displays "Hello, World!" 
+ * to the console.
+ *
+ * This is the first step in the HelloApp journey, where we start with a basic
+ * "Hello, World!" program. The application consists of a single class with a
+ * main method that serves as the entry point for the program. The main method
+ * uses the System.out.println() function to print the message "Hello, World!"
+ * to the console.
+ *
+ * @author Karunakaran R
+ * @version 1.0
+ */
+
+public class HelloApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
