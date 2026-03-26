9a7d413b8cf20d5b61f7edcfee9f32b22f4eb7aa
it-me-kkaran11 <rkarunakaran174@gmail.com>
2026-03-26 11:15:56 +0530

    UC7 Implemented Successfully

--- HelloApp.java
+++ HelloApp.java
@@ -1,20 +1,12 @@
-public class HelloArguments {
+public class HelloArgumentsJoin {
     public static void main(String[] args) {
-        // If no arguments are passed, print default greeting
+        
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            StringBuilder greeting = new StringBuilder("Hello, ");
             
-            // Enhanced for loop to append each argument with a comma
-            for (String name : args) {
-                greeting.append(name).append(", ");
-            }
-            
-            // Remove the trailing comma and space using substring
-            String finalGreeting = greeting.substring(0, greeting.length() - 2);
-            
-            System.out.println(finalGreeting);
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names);
         }
     }
 }
